e5651e40035f8cf2ff02360d64b86eab5838260a
Jonathan Bruckman <jbruckman86@gmail.com>
2022-09-25 15:59:54 -0400

    commit tree 70c34e74220423fbb44f48aefb251ba843c8e343

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
-// Code your solutions in this file
+// const names = ["Guadalupe", "Ollie", "Aki"]
+
+// function writeCards(name, event) {
+//     const message = []
+//     for (let i = 0; i < names.length; i++) {
+//        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`) 
+//     }
+//     return message
+// }
+
+// function countDown() {
+//     let countdown = 10;
+//     while (countdown >= 0) {
+//         console.log(countdown--)
+//     }
+// }
+
+// const names = ["Guadalupe", "Ollie", "Aki"]
+
+// function writeCards(name, event) {
+//     const message = []
+//     for (let i = 0; i < names.length; i++) {
+//         message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+//     }
+//     return message;
+// }
+
+// function countDown() {
+//     let countDown = 10
+//     while ( countDown >= 0) {
+//         console.log(countDown--)
+//     }
+// }
+
+// const names = ["Guadalupe", "Ollie", "Aki"]
+
+// function writeCards(name, event){
+//     const message = []
+//     for ( let i = 0; i < names.length; i++) {
+//     message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`) 
+//     }
+//     return message;
+// }
+
+// function countDown() {
+//     let countDown = 10
+//     while (countDown >= 0) {
+//         console.log(countDown--)
+//     }
+// }
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(name, event) {
+    let message = []
+    for (let i = 0; i < name.length; i++) {
+    message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return message
+}
+
+
+
+
+
+
